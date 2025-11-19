181c180d73022970e3f94c5a591a221474dc1be0
johannesmx <johannes.muljana@ait.nsw.edu.au>
2025-11-19 08:43:50 +0000

    adding php and database

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include "fragments/head.php"; ?>
+
+<body>
+    <?php include "fragments/pageheader.php"; ?>
+    <!-- carousel -->
+    <div class="main-carousel" data-flickity='{ "autoPlay": true, "cellAlign": "left", "contain": true }'>
+        <div class="slide">
+            <h2>Black Friday Sale</h2>
+            <a href="#" class="slide-button">Shop Black Friday</a>
+        </div>
+        <div class="slide">2</div>
+        <div class="slide">3</div>
+        <div class="slide">4</div>
+    </div>
+    <main class="content">
+        <div class="card">
+            <img src="/images/categories/womens.jpg" alt="a woman touching grass">
+            <div class="card-content">
+                <h3>Womens</h3>
+                <a href="#" class="card-button">Shop Womens</a>
+            </div>
+        </div>
+         <div class="card">
+            <img src="/images/categories/mens.jpg" alt="a man doing push ups">
+            <div class="card-content">
+                <h3>Mens</h3>
+                <a href="#" class="card-button">Shop Mens</a>
+            </div>
+        </div>
+        <div class="card">
+            <img src="/images/categories/kids.jpg" alt="a child running in a baseball game">
+            <div class="card-content">
+                <h3>Kids</h3>
+                <a href="#" class="card-button">Shop Kids</a>
+            </div>
+        </div>
+        <div class="banner">
+            <h2>Sale</h2>
+            <a class="slide-button" href="#">Shop Sale</a>
+        </div>
+        <div class="card">
+            <img src="/images/categories/womens.jpg" alt="a woman touching grass">
+            <div class="card-content">
+                <h3>Womens</h3>
+                <a href="#" class="card-button">Shop Womens</a>
+            </div>
+        </div>
+         <div class="card">
+            <img src="/images/categories/mens.jpg" alt="a man exercising">
+            <div class="card-content">
+                <h3>Mens</h3>
+                <a href="#" class="card-button">Shop Mens</a>
+            </div>
+        </div>
+        <div class="card">
+            <img src="/images/categories/kids.jpg" alt="child playing baseball">
+            <div class="card-content">
+                <h3>Kids</h3>
+                <a href="#" class="card-button">Shop Kids</a>
+            </div>
+        </div>
+    </main>
+    
+    <?php include "fragments/footer.php"; ?>
+</body>
+
+</html>
