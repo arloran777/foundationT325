b47dc8053a713079de9ac82a3d4daff27ef92a56
Charlie <josemaciastraductor@gmail.com>
2025-11-19 08:44:33 +0000

    adding php pages and database

---
+++ fragments/pageheader.php
@@ -0,0 +1,38 @@
+<header class="main-header">
+        <button type="button" class="nav-button">
+            <i class="fa-solid fa-bars"></i>
+        </button>
+        <a href="/">
+            <img class="logo" src="images/ProGearHubLogo.png" alt="progear hub logo">
+        </a>
+        <form class="search" method="get" action="/search.php">
+            <input type="search" placeholder="Search product">
+            <button>
+                <img src="images/search_40dp_4A3D28_FILL0_wght400_GRAD0_opsz40.png" alt="search icon">
+            </button>
+        </form>
+        <nav class="user-nav">
+            <a href="stores.html">
+                <span class="material-symbols-outlined">
+                    storefront
+                </span>
+            </a>
+            <a href="account.html">
+                <span class="material-symbols-outlined">
+                    person
+                </span>
+            </a>
+            <a href="cart.html">
+                <span class="material-symbols-outlined">
+                    shopping_cart
+                </span>
+            </a>
+        </nav>
+        <nav class="shop-nav">
+            <a href="womens.html">Womens</a>
+            <a href="mens.html">Mens</a>
+            <a href="kids.html">Kids</a>
+            <a href="sports.html">Sports</a>
+            <a href="aboutus.html">About Us</a>
+        </nav>
+    </header>
---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include "fragments/head.php"; ?>
+
+<body>
+    <?php include "fragments/pageheader.php"; ?>
+    <!-- carousel -->
+    <div class="main-carousel" data-flickity='{ "autoPlay": true, "cellAlign": "left", "contain": true }'>
+        <div class="slide">
+            <h2>Unleash the power</h2>
+            <a href="#" class="slide-button">Explore our new poducts</a>
+        </div>
+        <div class="slide">Black Friday Sales</div>
+        <div class="slide">Dive into our discounts</div>
+        <div class="slide">Chistmas Sales</div>
+    </div>
+    <main class="content">
+        <div class="card">
+            <img src="/images/sports/WomanPlayingTenis.webp" alt="a woman playing tennis">
+            <div class="card-content">
+                <h3>Womens</h3>
+                <a href="#" class="card-button">Shop Womens</a>
+            </div>
+        </div>
+         <div class="card">
+            <img src="/images/mens/ropa-hombre-yoga.jpg" alt="a man doing yoga">
+            <div class="card-content">
+                <h3>Mens</h3>
+                <a href="#" class="card-button">Shop Mens</a>
+            </div>
+        </div>
+        <div class="card">
+            <img src="/images/kids sports.jpeg" alt="kids on sprotswear">
+            <div class="card-content">
+                <h3>Kids</h3>
+                <a href="#" class="card-button">Shop Kids</a>
+            </div>
+        </div>
+        <div class="banner">
+            <h2>Sale</h2>
+            <a class="slide-button" href="#">Sports Equipment</a>
+        </div>
+        <div class="card">
+            <img src="/images/sports/WomanPlayingTenis.webp" alt="a woman playing tennis">
+            <div class="card-content">
+                <h3>Womens</h3>
+                <a href="#" class="card-button">Shop Womens</a>
+            </div>
+        </div>
+         <div class="card">
+            <img src="/images/mens/ropa-hombre-yoga.jpg" alt="a man doing yoga">
+            <div class="card-content">
+                <h3>Mens</h3>
+                <a href="#" class="card-button">Shop Mens</a>
+            </div>
+        </div>
+        <div class="card">
+            <img src="/images/kids sports.jpeg" alt="kids on sportswear">
+            <div class="card-content">
+                <h3>Kids</h3>
+                <a href="#" class="card-button">Shop Kids</a>
+            </div>
+        </div>
+    </main>
+    <?php include "fragments/footer.php"; ?>
+</body>
+
+</html>
