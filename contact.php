9c77c049e47905a6917cbb19c75b44eb9372689f
johannesmx <johannes.muljana@ait.nsw.edu.au>
2025-11-26 07:07:34 +0000

    adding database connection in contact

--- contact.php
+++ contact.php
@@ -1,3 +1,23 @@
+<?php include "src/database.php"; 
+// check for form submission
+if( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
+    // get data from form
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+    // insert the data into contact_us table
+    // SQL query
+    $query = "INSERT INTO contact_us (name,email,subject,message,submitted_at)
+            VALUES(?,?,?,?,NOW() )
+    ";
+    $statement = $connection -> prepare($query);
+    $statement -> bind_param("ssss",$name,$email,$subject,$message);
+    if( !$statement -> execute() ) {
+        // there's an error
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,25 +25,18 @@
 
 <body>
     <?php include "fragments/pageheader.php"; ?>
-    <!-- carousel -->
-    <!-- <div class="main-carousel" data-flickity='{ "autoPlay": true, "cellAlign": "left", "contain": true }'>
-        <div class="slide">
-            <h2>Black Friday Sale</h2>
-            <a href="#" class="slide-button">Shop Black Friday</a>
-        </div>
-        <div class="slide">2</div>
-        <div class="slide">3</div>
-        <div class="slide">4</div>
-    </div> -->
+    
     <main class="content">
         <div class="card">
             <div class="card-content">
-                <form id="contact" method="post" action="/contact">
+                <form id="contact" method="post" action="/contact.php">
                     <h1>Contact Us</h1>
                     <label for="name">Name</label>
                     <input required id="name" name="name" type="text" placeholder="Gemma Smith">
                     <label for="email">Email</label>
                     <input required id="email" name="email" type="email" placeholder="gemma@example.com">
+                    <label for="subject">Subject</label>
+                    <input required id="subject" name="subject" type="text" placeholder="inquiry">
                     <label for="message">Message</label>
                     <textarea required id="message" name="message" rows="5" cols="30" placeholder="Hello there"></textarea>
                     <button type="submit" class="form-button">Send</button>
